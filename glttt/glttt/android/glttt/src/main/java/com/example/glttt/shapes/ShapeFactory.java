93f8cc2bb52397a771abbb1fa2d335467b775de2
510codes <510codes@gmail.com>
2015-02-21 16:34:47 -0800

    add initial simple sphere implementation

--- glttt/glttt/android/glttt/src/main/java/com/example/glttt/shapes/ShapeFactory.java
+++ glttt/glttt/android/glttt/src/main/java/com/example/glttt/shapes/ShapeFactory.java
@@ -44,4 +44,85 @@ public class ShapeFactory {
 
         return t;
     }
+
+    public Triangle[] createSphere( String id, float radius, int rings, int sectors, float[] colour, float vertexDivideFactor ) {
+        float[] vertices;
+        float[] normals;
+        float[] texcoords;
+        int[] indices;
+        final float R = 1.0f/(float)(rings-1);
+        final float S = 1.0f/(float)(sectors-1);
+        int r, s;
+
+        vertices = new float[(rings * sectors * 3)];
+        normals = new float[(rings * sectors * 3)];
+        texcoords = new float[(rings * sectors * 2)];
+        int t = 0;
+        int v = 0;
+        int n = 0;
+        for(r = 0; r < rings; r++) for(s = 0; s < sectors; s++) {
+            final float y = (float)Math.sin( -(Math.PI / 2.0f) + Math.PI * r * R );
+            final float x = (float)Math.cos(2*Math.PI * s * S) * (float)Math.sin( Math.PI * r * R );
+            final float z = (float)Math.sin(2*Math.PI * s * S) * (float)Math.sin( Math.PI * r * R );
+
+            texcoords[t] = s*S;
+            t++;
+
+            texcoords[t] = r*R;
+            t++;
+
+            vertices[v] = x * radius;
+            v++;
+
+            vertices[v] = y * radius;
+            v++;
+
+            vertices[v] = z * radius;
+            v++;
+
+            normals[n] = x;
+            n++;
+
+            normals[n] = y;
+            n++;
+
+            normals[n] = z;
+            n++;
+        }
+
+        indices = new int[(rings * sectors * 4)];
+        int i = 0;
+        for (r=0; r < rings - 1; r++) for(s = 0; s < sectors-1; s++) {
+            indices[i] = r * sectors + s;
+            i++;
+
+            indices[i] = r * sectors + (s+1);
+            i++;
+
+            indices[i] = (r+1) * sectors + (s+1);
+            i++;
+
+            indices[i] = (r+1) * sectors + s;
+            i++;
+        }
+
+        return null;
+    }
+
+    /*void draw(GLfloat x, GLfloat y, GLfloat z)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslatef(x,y,z);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_NORMAL_ARRAY);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+        glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
+        glNormalPointer(GL_FLOAT, 0, &normals[0]);
+        glTexCoordPointer(2, GL_FLOAT, 0, &texcoords[0]);
+        glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_SHORT, &indices[0]);
+        glPopMatrix();
+    }*/
 }
